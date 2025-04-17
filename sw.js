f23641991ebfc4f6abf7e70da7464be1cb268d0b
Danial <75368856+DanialJ-Dev@users.noreply.github.com>
2025-04-18 00:38:01 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
 self.addEventListener("notificationclick", function(event) {
-    event.notification.close();
-    clients.openWindow("/"); // باز کردن صفحه اپلیکیشن در کلیک روی نوتیفیکیشن
-  });
+  event.notification.close();
+  const targetUrl = "https://danialj-dev.github.io/todo-app-js/";
   
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then(function(clientList) {
+      for (const client of clientList) {
+        if (client.url === targetUrl && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(targetUrl);
+      }
+    })
+  );
+});
